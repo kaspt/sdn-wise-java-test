09967a81dddc55224db540a339d36e004d1b9600
Unknown <tobias.kasper@hotmail.com>
2019-05-22 10:52:25 +0200

    init maven project

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+    public static void main(final String[] args) {
+
+        System.out.println("Test start");
+    }
+
+}
